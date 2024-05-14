b76e4fd2f97d3c56a2f0d1677e39364f95db7d60
박성재 <sem201@naver.com>
2024-05-14 15:29:15 +0900

    과제 1번 컷~

--- project/static/js/main/main_page.js
+++ project/static/js/main/main_page.js
@@ -1,1 +1,14 @@
 /* main page - JS */
+$(document).ready(function() {
+    $('#item_list .item:lt(3)').addClass('active');
+
+    $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        var windowHeight = $(this).height();
+        var documentHeight = $(document).height();
+
+        if (scrollTop + windowHeight == documentHeight) {
+            $('#item_list .item.active:last').nextAll('.item').slice(0, 3).addClass('active');
+        }
+    });
+});
